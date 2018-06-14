73dde3ac59b03113de6b0e66b36078bb49fbee1a
Lewis <liu.xiao1988@gmail.com>
2018-06-13 23:47:59 -0700

    Fix the script chmod location with task folder

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 mkdir /tmp/cflow
 gsutil cp -r gs://trackml/cflow/* /tmp/cflow
 export GOOGLE_APPLICATION_CREDENTIALS=/tmp/cflow/credential/cloudlewisvpn-14cbd880715c.json
-chmod +x /tmp/cflow/*.sh
+chmod +x /tmp/cflow/task/*.sh
 python3 /tmp/cflow/startup-signal.py &
